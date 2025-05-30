5051ea9ea3502d7023b3fadff6c6c3f295e28b21
Fabio Santos <f09santos@gmail.com>
2025-05-30 11:49:07 -0300

    atualizações de dados temporadas e episodios

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosSeries;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,13 +14,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=6a0f270b");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSeries dados = conversor.obterDados(json, DadosSeries.class);
-		System.out.println(dados);
-
+		Principal principal = new Principal();
+		principal.exibeMenu();
 
 	}
 }
